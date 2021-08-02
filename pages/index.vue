c4ec1b9d51d70830695d184c2480dbdef8f1dfd6
hzm <467251648@qq.com>
2021-08-02 21:04:20 +0800

    暂提处理

--- pages/index.vue
+++ pages/index.vue
@@ -49,14 +49,15 @@ export default {
             menuRoute: [] // 目录数据
         };
     },
-    asyncData({ app: { router } }) {
+    asyncData({ $axios, query, param, store }) {
         // console.log("上下文对象:%o", ctx);
         return {
-            rootInfo: router
+            // rootInfo: router
         };
     },
     mounted() {
         this.initRoute(); // 处理目录数据
+        // console.log("this实例:%o", this);
         // inject $ 挂载查询
         // console.log(this.$info.searchInfo("name"), "信息");
     },
@@ -68,17 +69,136 @@ export default {
 
         async initRoute() {
             // 获取整个项目目录
-            // this.rootInfo = this.context.app.router.options.routes;
-            console.log("项目目录", this.rootInfo);
-            let tempList = Array.isArray(this.rootInfo.options.routes);
-            let _resetFunc = (route, index = 0, parent = {}) => {
-                let hasTimes = route.split("/").length; // 根据 / 判断目录所在的等级
-                if (hasTimes > 1) {
-                    // 二三级目录
-                } else {
-                    // 一级目录
+            this.rootInfo = this.$router.options.routes;
+            let tempList = Array.from(this.rootInfo); // .slice(0, 3)
+            tempList.filter(root => {
+                let chilrenList = root.name.split("-");
+                root.index = chilrenList.length; // 根据-含有个数确定层级
+                root.indexInfo = root.name;
+                this._resetFunc(root, root.index, {});
+            });
+            console.log(this.menuRoute, "处理结果");
+        },
+
+        async _resetFunc(route, index = 0, parentInfo = {}) {
+            // 根据 - 个数判断目录等级 1个就是1级目录 俩个是二级目录 以此类推
+            console.log(route, index, parentInfo, "传进来的route信息");
+
+            // 第一层级
+            if (index == 1) {
+                // 有子级的
+                console.log(route, "第一层级的数据");
+                let findIndex = this.menuRoute.findIndex(
+                    root => root.name == route.name
+                );
+                // 检验为不存在再推入数组中
+                if (route?.path && findIndex == -1) {
+                    this.menuRoute.push({
+                        index: index,
+                        name: route.name,
+                        type: "document",
+                        list: [
+                            {
+                                id: route.name,
+                                path: route.path,
+                                index: index + 1
+                            }
+                        ]
+                    });
+                } else if (findIndex == -1) {
+                    console.log(this.menuRoute, route.parentId, "菜单路由");
+                    this.menuRoute.push({
+                        index: index,
+                        name: route.name,
+                        type: "folder",
+                        list: []
+                    });
                 }
-            };
+            } else if (route?.parentId) {
+                // 存在父级的
+                console.log(route, "存在父级信息的详情");
+                let findIndex = this.menuRoute.findIndex(
+                    root => root.name == route.parentId
+                );
+                if (findIndex > -1) {
+                    this.menuRoute[findIndex].list.push({
+                        ...route
+                    });
+                }
+            } else if (route?.indexInfo) {
+                // 其他层级
+                console.log("一级以上层级传进来的信息:%o", route);
+                let childrenList = route.indexInfo.split("-");
+                for (let i = 0; i < childrenList.length; i++) {
+                    console.log(childrenList[i], "遍历");
+                    let currentInfo = {
+                        name: childrenList[i],
+                        index: i + 1
+                    };
+                    if (i > 0) {
+                        currentInfo.parentId = childrenList[i - 1];
+                        currentInfo.indexName = childrenList
+                            .slice(0, i + 1)
+                            .join("-");
+                    }
+
+                    if (i == childrenList.length - 1) {
+                        currentInfo.path = route.path;
+                    }
+
+                    this._resetFunc(currentInfo, i + 1, {});
+                }
+
+                // let findIndex = this.menuRoute.findIndex(
+                //     root => root.id == route.name
+                // );
+            }
+
+            // 是二三级目录的
+            // if (chilrenList.length > 1) {
+            //     for (let i = 0; i < chilrenList.length; i++) {
+            //         let currentRoute,
+            //             parentRoute = {};
+            //         if (i > 0) {
+            //             // currentRoute = route;
+            //             parentRoute = {
+            //                 index: index,
+            //                 id: chilrenList.slice(0, i).join("-")
+            //             };
+            //         }
+            //         currentRoute = {
+            //             name: chilrenList[i]
+            //         };
+            //         // console.log(parentRoute, "currentRoute整理");
+            //         this._resetFunc(currentRoute, i, parentRoute);
+            //     }
+            // }
+            // //  else if (chilrenList.length == 1) {
+            // //     this.menuRoute.push({
+            // //         index: index,
+            // //         ...route,
+            // //         list: []
+            // //     });
+            // // }
+
+            // let findIndex = this.menuRoute.findIndex(
+            //     root => root.name == parentInfo.id
+            // );
+
+            // if (findIndex > -1) {
+            //     this.menuRoute[findIndex].list.push({
+            //         ...route,
+            //         type: 2,
+            //         index: index
+            //     });
+            // } else if (!chilrenList.length) {
+            //     this.menuRoute.push({
+            //         index: 0,
+            //         type: 1,
+            //         name: route.name,
+            //         list: [{ name: route.name, path: route.path, index: index }]
+            //     });
+            // }
         }
     }
 };
---
+++ pages/test/symbol.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- 新建文件的模板 -->
+    <div class="example">
+        <div v-for="(value, key) in testObj" :key="key">
+            {{ key }}：{{ value }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: "symbol",
+            meta: "",
+            name: "symbol",
+            script: []
+        };
+    },
+    data() {
+        return {
+            testObj: {
+                fate: "asf1"
+            }
+        };
+    },
+    mounted() {
+        this.doFunc();
+    },
+    methods: {
+        doFunc() {
+            this.testObj.name = "1234";
+            this.testObj[Symbol] = "foo";
+            this.testObj.SymbolKey = Symbol("hello");
+            console.log(this.testObj, "testObj");
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import "~assets/less/index.less";
+.example {
+}
+</style>

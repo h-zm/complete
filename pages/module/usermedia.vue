fc8734363955f15e60650b9e96d058dc36ef5e35
hzm <467251648@qq.com>
2023-08-24 21:40:56 +0800

    feat: 测试使用RTCpeerConnection

--- pages/module/usermedia.vue
+++ pages/module/usermedia.vue
@@ -1,11 +1,7 @@
 <template>
     <!-- 新建文件的模板 -->
     <div class="page">
-        <contents>
-            <codes>webrtc</codes> (Web Real-Time Communications)
-            是一项实时通讯技术，它允许网络应用或者站点，在不借助中间媒介的情况下，建立浏览器之间点对点（Peer-to-Peer）的连接，实现视频流和（或）音频流或者其他任意数据的传输。WebRTC
-            包含的这些标准使用户在无需安装任何插件或者第三方的软件的情况下，创建点对点（Peer-to-Peer）的数据分享和电话会议成为可能。
-        </contents>
+        <contents> <codes>usermedia</codes> 分享屏幕视频流信息 </contents>
 
         <div class="page__area">
             <video
